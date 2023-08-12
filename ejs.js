8683b063c3bf9736be522fbddb7536df2d43c782
hassan2314 <hassan87691@gmail.com>
2023-08-12 17:30:36 +0500

    'third'

--- ejs.js
+++ ejs.js
@@ -13,11 +13,14 @@ app.set('view engine', 'ejs');
 app.get('/profile', (req, res) => {
     const user=
        { name:"Hassan",
-        age: 22}
+        age: 22,
+        skills:['php','js','c++']}
     
   res.render('profile',{user});
 });
-
+app.get('/login',(_,res)=>{
+  res.render('login')
+})
 app.listen(4500, () => {
   console.log('Server is running on port 4500');
 });
